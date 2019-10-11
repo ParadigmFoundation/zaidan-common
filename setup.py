79cfcaa039694798cdb27eea07ed451a2edf8c7f
Henry Harder <henry@paradigm.market>
2019-10-11 16:53:22 -0700

    export FlaskLogger

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='zaidan',
-    version='0.0.2',
+    version='0.0.4',
     author="Henry Harder",
     author_email="henry@paradigm.market",
     description="Common utilities for the Zaidan system.",
