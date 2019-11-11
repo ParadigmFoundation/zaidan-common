790a50483587fa9c776dcaec77ffea260b3ad259
Henry Harder <henry@paradigm.market>
2019-11-11 15:05:27 -0800

    add test script

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 import setuptools
+import unittest
+
+
+def get_test_suite():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('test', pattern='*_test.py')
+    return test_suite
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='zaidan',
-    version='0.0.20',
+    version='0.0.21',
     author="Henry Harder",
     author_email="henry@paradigm.market",
     description="Common utilities for the Zaidan system.",
     long_description=long_description,
+    test_suite="setup.get_test_suite",
     long_description_content_type="text/markdown",
     url="https://github.com/ParadigmFoundation/zaidan-common",
     packages=setuptools.find_packages(),
@@ -17,7 +26,7 @@ setuptools.setup(
     install_requires=[
         "json-logging",
         "redis",
-        "mysql.connector",
+        "mysql-connector",
         "requests"
     ],
     classifiers=[
